9d788ed3626c2a9b35bc9ea2412176ed18bdf965
Marc Backes <mab@mab.lu>
2020-02-28 14:26:46 +0100

    removed some stuff

--- src/routers/demo.router.js
+++ src/routers/demo.router.js
@@ -6,7 +6,7 @@ router.get('/random', async (req, res) => {
 
     try {
         const random = Math.random()
-        return res.status(201).send({random})
+        return res.status(201).send({random, secret: process.env.SECRET})
     } catch (error) {
         res.status(400).send({error: 'Oh no! An error!', error: error.message})
     }
